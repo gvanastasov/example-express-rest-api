f422974ef4315e97066cd8d957ee88b00fe2e51f
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-05-30 22:35:08 +0200

    add swagger user schema

--- data/models/user-model.js
+++ data/models/user-model.js
@@ -1,5 +1,22 @@
 const mongoose = require("mongoose");
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     user:
+ *       type: object
+ *       properties:
+ *         username:
+ *           type: string
+ *           description: The first name of the user.
+ *         email:
+ *           type: string
+ *           description: The email of the user.
+ *       example:
+ *         username: admin
+ *         email: admin@mail.com
+ */
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true },
