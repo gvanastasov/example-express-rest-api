16503bf055aae258a13af18b346b8b30a3690250
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-05-30 22:30:48 +0200

    adjust resource pattern implementation

--- plugins/resource.js
+++ plugins/resource.js
@@ -1,10 +1,8 @@
-const resource = function (app, controller) {
-    this.get(path, controller.index);
-    this.get(`${path}/:id`, controller.show);
-    this.delete(`${path}/:id`, function (req, res) {
-        var id = parseInt(req.params.id, 10);
-        controller.destroy(req, res, id);
-    });
+const resource = function (resource, controller) {
+    this.get(resource, controller.get);
+    this.get(`${resource}/:id`, controller.getById);
+    this.post(`${resource}`, controller.create);
+    this.delete(`${resource}/:id`, controller.delete);
 };
 
 const plugin = function (app) {
