29659c1a729d4ba64adc7b3a4c0cc1332a9d3b30
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-05-31 23:35:30 +0200

    use middleware for resource identifier

--- controllers/v1/index.js
+++ controllers/v1/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 
 const { install } = require("../../plugins");
+const { convertParamToInteger } = require("../../middleware/converter-middleware");
+const { isPositive } = require("../../middleware/validation-middleware");
 
 const api_v1 = express.Router();
 install(api_v1, "resource");
 
 api_v1.resource("/users", require("./users-controller"));
+
+api_v1.param("id", convertParamToInteger);
+api_v1.param("id", isPositive);
 
 module.exports = api_v1;
 
