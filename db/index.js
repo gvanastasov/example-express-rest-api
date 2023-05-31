0dd117e699c37005c204e93180f9137f04885b1c
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-05-31 15:42:20 +0200

    add sqlite3 db

--- db/index.js
+++ db/index.js
@@ -1,5 +1,27 @@
-const initDb = async function() {
-    
+const Database = require("better-sqlite3");
+
+let db = null;
+
+const startDb = function() {
+    try {
+        db = new Database(":memory:", { verbose: console.log });
+        console.log("Connected to the in-memory SQlite database.");
+    } catch (err) {
+        console.error(err?.message);
+    }
 };
 
-module.exports = { initDb };
+const stopDb = function() {
+    if (!db) {
+        return console.log("Database not initialized.");
+    }
+
+    try {
+        db.close();
+        console.log("Connection to the in-memory SQlite database closed.");
+    } catch (err) {
+        console.error(err?.message);
+    }
+};
+
+module.exports = { db, startDb, stopDb };
